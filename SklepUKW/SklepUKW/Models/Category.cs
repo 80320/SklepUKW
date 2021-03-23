07b6fa3c623f669f0e5824454aa1733aed0aaf51
80320 <erybacki@interia.eu>
2021-03-23 10:29:04 +0100

    Drugie zajęcia - część pierwsza

---
+++ SklepUKW/SklepUKW/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SklepUKW.Models
+{
+    public class Category
+    {
+        public int idCategory { get; set; }
+
+        [Required(ErrorMessage = "Podaj nazwę kategorii")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public string Desc { get; set; }
+
+        public ICollection<Film> Films { get; set; }
+
+
+    }
+}
---
+++ SklepUKW/SklepUKW/Models/Film.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace SklepUKW.Models
+{
+    public class Film
+    {
+        
+        public int FilmID { get; set; }
+
+        [Required(ErrorMessage = "Wpisz tytuł filmu")]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+
+        public string Desc { get; set; }
+
+        [Required(ErrorMessage = "Podaj cenę filmu")]
+        public decimal Price { get; set; }
+
+        public DateTime AddDate { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
