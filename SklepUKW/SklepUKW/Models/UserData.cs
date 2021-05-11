c3eaae584aa1abccac4105b1f52a029b85e7730b
80320 <erybacki@interia.eu>
2021-05-11 11:14:20 +0200

    Lab 11.05 - Logowanie i Rejestracja

---
+++ SklepUKW/SklepUKW/Models/UserData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SklepUKW.Models
+{
+    public class UserData
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        [EmailAddress(ErrorMessage = "Błędny adres email!")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "Podaj poprawny numer telefonu!")]
+        public string PhoneNumber { get; set; }
+
+    }
+}
---
+++ SklepUKW/SklepUKW/ViewModels/AccountViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SklepUKW.ViewModels
+{
+    public class LoginViewModel
+    {
+
+    }
+
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Musisz podać Email!")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        [Required(ErrorMessage = "Musisz podać hasło!")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło:")]
+        [Required(ErrorMessage = "Musisz podać oba hasła!")]
+        [Compare("Password", ErrorMessage = "Hasła muszą być jednakowe!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
